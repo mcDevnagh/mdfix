9e69679423dd6c91aae44f2aaf992878aae5537c
Michael McDonagh <michael@mcdonagh.dev>
2024-02-11 13:59:01 -0500

    Don't break links

--- fixers/whitespace.go
+++ fixers/whitespace.go
@@ -43,8 +43,16 @@ func (f *Whitespace) cut(line text.Segment, source []byte) {
 	target_len := line.Start + f.TextWidth
 	if line.Stop > target_len {
 		var stop int
+		bracket_stack := 0
+		parenthesis_stack := 0
+		for stop = line.Stop - 1; stop > target_len; stop-- {
+			stack(&bracket_stack, &parenthesis_stack, source[stop])
+		}
+
 		for stop = target_len; stop > line.Start; stop-- {
-			if unicode.IsSpace(rune(source[stop])) {
+			if stack(&bracket_stack, &parenthesis_stack, source[stop]) {
+				continue
+			} else if unicode.IsSpace(rune(source[stop])) && bracket_stack == 0 && parenthesis_stack == 0 {
 				source[stop] = '\n'
 				break
 			}
@@ -53,5 +61,29 @@ func (f *Whitespace) cut(line text.Segment, source []byte) {
 		if stop != line.Start {
 			f.cut(text.NewSegment(stop, line.Stop), source)
 		}
+	}
+}
+
+func stack(bracket_stack, parenthesis_stack *int, char byte) bool {
+	if char == ']' {
+		*bracket_stack++
+		return true
+	} else if char == '[' {
+		*bracket_stack--
+		if *bracket_stack < 0 {
+			*bracket_stack = 0
+		}
+		return true
+	} else if char == ')' {
+		*parenthesis_stack++
+		return true
+	} else if char == '(' {
+		*parenthesis_stack--
+		if *parenthesis_stack < 0 {
+			*parenthesis_stack = 0
+		}
+		return true
+	} else {
+		return false
 	}
 }
